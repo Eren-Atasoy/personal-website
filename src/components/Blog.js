1aa607a775825fc1d824228463ee5314d87a5377
Eren Atasoy <eratasoy@k1m06s07.42istanbul.com.tr>
2025-10-13 18:30:05 +0300

    fix

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Calendar, Clock, ArrowRight, Tag, Loader, ExternalLink } from 'lucide-react';
+import { Calendar, Clock, Tag, Loader, ExternalLink } from 'lucide-react';
 import { fetchMediumArticles } from '../services/mediumService';
 
 const BlogCard = ({ post }) => (
